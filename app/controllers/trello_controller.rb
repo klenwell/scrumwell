13dda0cdadd32b945ee61e82ede2fe465737b0dc
klenwell <klenwell@gmail.com>
2018-06-21 20:32:21 -0700

    Lists boards sorted by last update.

--- app/controllers/trello_controller.rb
+++ app/controllers/trello_controller.rb
@@ -1,5 +1,6 @@
 class TrelloController < ApplicationController
   def boards_index
     @trello = TrelloService.new
+    @active_boards = @trello.boards.keep_if{ |b| b.last_activity_date.present? }
   end
 end
