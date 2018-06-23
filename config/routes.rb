e21e813b951a093eeb830786c25852584f430080
klenwell <klenwell@gmail.com>
2018-06-22 20:21:17 -0700

    Adds board show view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/trello/boards', to: 'trello#boards_index'
+  get '/trello/boards/:id', to: 'trello#boards_show', as: 'trello_boards_show'
   get '/trello/orgs', to: 'trello#orgs_index'
   get '/trello/orgs/:id/boards', to: 'trello#orgs_boards_index', as: 'trello_orgs_boards'
 
