7210f90de13359232cba39ef95bfc60c23ab148e
klenwell <klenwell@gmail.com>
2018-06-28 22:01:52 -0700

    Adds scrummy board method to ScrumBacklog model.

--- app/models/scrum_backlog.rb
+++ app/models/scrum_backlog.rb
@@ -1,2 +1,14 @@
 class ScrumBacklog < ApplicationRecord
+  # Class Methods
+  def self.scrummy_trello_board?(trello_board)
+    # A scrummy board will contain these lists: wish heap, backlog, current
+    scrummy_list_names = ['wish heap', 'backlog', 'current']
+    board_list_names = trello_board.lists.map { |list| list.name.downcase.strip }
+
+    scrummy_list_names.each do |required_name|
+      return false unless board_list_names.any? { |list_name| list_name.include? required_name }
+    end
+
+    true
+  end
 end
--- test/models/scrum_backlog_test.rb
+++ test/models/scrum_backlog_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class ScrumBacklogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "expects trello board to be identified as scrummy" do
+    # Arrange
+    scrummy_board = Trello::Board.new(id: 'scrummy-board')
+    lists = ['wish heap', 'backlog', 'current'].map { |name| Trello::List.new(name: name) }
+    scrummy_board.stubs(:lists).returns(lists)
+
+    # Act
+    is_scrummy = ScrumBacklog.scrummy_trello_board?(scrummy_board)
+
+    # Assert
+    assert is_scrummy
+  end
+
+  test "expects trello board NOT to be identified as scrummy" do
+    # Arrange
+    scrummy_board = Trello::Board.new(id: 'non-scrummy-board')
+    lists = ['Todo', 'Doing', 'Done'].map { |name| Trello::List.new(name: name) }
+    scrummy_board.stubs(:lists).returns(lists)
+
+    # Act
+    is_scrummy = ScrumBacklog.scrummy_trello_board?(scrummy_board)
+
+    # Assert
+    assert_not is_scrummy
+  end
 end
