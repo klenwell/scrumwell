757199e6ea55d5a1ab9e57a1d8a3b09f4b220785
klenwell <klenwell@gmail.com>
2018-07-02 20:03:27 -0700

    Adds scrum sprint show view.

--- app/controllers/scrum/sprints_controller.rb
+++ app/controllers/scrum/sprints_controller.rb
@@ -1,9 +1,28 @@
 module Scrum
   class SprintsController < ApplicationController
+    before_action :set_scrum_sprint, only: [:show, :edit, :update, :destroy]
+
     # GET /scrum_sprints
     # GET /scrum_sprints.json
     def index
       @scrum_sprints = ScrumSprint.all
+    end
+
+    # GET /scrum_sprints/1
+    # GET /scrum_sprints/1.json
+    def show; end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_scrum_sprint
+      @scrum_sprint = ScrumSprint.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def scrum_backlog_params
+      params.require(:scrum_sprint).permit(:scrum_backlog_id, :trello_list_id, :trello_pos,
+                                           :name, :started_on, :ended_on)
     end
   end
 end
