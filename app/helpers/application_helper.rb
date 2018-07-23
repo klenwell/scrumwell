e41e64144bb1653a40389d1db94e4f45dda40304
klenwell <klenwell@gmail.com>
2018-07-22 21:14:13 -0700

    Replaces helpers in scrum sprint form with bootstrap form helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,25 +25,4 @@ module ApplicationHelper
     alt = opts.fetch(:alt, 'Not Available')
     value.presence || tag.span(alt, class: 'text-muted')
   end
-
-  # Source: https://stackoverflow.com/a/27209195/1093087
-  # rubocop: disable Metrics/AbcSize
-  def bootstrap_errors_for(object)
-    return nil unless object.errors.any?
-
-    tag.div(class: "card border-danger error-explanation mb-3") do
-      concat(tag.div(class: "card-header bg-danger text-white") do
-        f = '%s prohibited this %s from being saved:'
-        concat format(f, pluralize(object.errors.count, "error"), object.class.name.downcase)
-      end)
-      concat(tag.div(class: "card-body") do
-        concat(tag.ul(class: 'mb-0') do
-          object.errors.full_messages.each do |msg|
-            concat tag.li(msg)
-          end
-        end)
-      end)
-    end
-  end
-  # rubocop: enable Metrics/AbcSize
 end
--- app/helpers/scrum/sprints_helper.rb
+++ app/helpers/scrum/sprints_helper.rb
@@ -4,4 +4,12 @@ module Scrum::SprintsHelper
     return 'N/A' if sprint.average_velocity.nil?
     format('%.1f', sprint.average_velocity)
   end
+
+  def scrum_sprint_form_url(scrum_sprint)
+    if scrum_sprint.new_record?
+      scrum_board_sprints_path(scrum_sprint.scrum_board, scrum_sprint)
+    else
+      scrum_sprint
+    end
+  end
 end
