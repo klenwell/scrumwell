850a1a490a358a1dfef35b16987a8797d3e70439
klenwell <klenwell@gmail.com>
2018-08-05 17:45:46 -0700

    Dynamically updates sprint table row after import button clicked.

--- app/assets/javascripts/scrum/sprints.js
+++ app/assets/javascripts/scrum/sprints.js
@@ -4,6 +4,14 @@ var ScrumSprint = (function() {
 
   // Constants
   var IMPORT_ICON_SELECTOR = 'table.sprints a.import-sprint';
+  var TABLE_ROW_FIELDS = [
+    'story_points_completed',
+    'average_velocity',
+    'average_story_size',
+    'backlog_story_points',
+    'wish_heap_story_points',
+    'notes'
+  ];
 
   // Module Vars
 
@@ -14,24 +22,57 @@ var ScrumSprint = (function() {
 
   // Private Methods
   var initImportHandler = function() {
-    $(IMPORT_ICON_SELECTOR).on('ajax:success', updateTableRow)
-                           .on('ajax:error', ajaxError);
+    // https://github.com/rails/jquery-ujs/wiki/ajax
+    $(IMPORT_ICON_SELECTOR).on('ajax:send', clearTableRow)
+                           .on('ajax:success', updateTableRow)
+                           .on('ajax:error', restoreTableRow);
   };
 
-  var updateTableRow = function(event) {
-    // For rails-ujs: https://stackoverflow.com/a/45632235/1093087
-    var detail = event.detail;
-    var data = detail[0], status = detail[1], xhr = detail[2];
-
+  var clearTableRow = function(event) {
     var $importButton = $(this);
     var $row = $importButton.closest('tr');
 
-    console.log(event, data);
+    $importButton.fadeTo('slow', 0.5);
+
+    $.each(TABLE_ROW_FIELDS, function(i, field) {
+      var tdSelector = `td.${field}`;
+      var $td = $row.find(tdSelector);
+      $td.fadeTo('slow', 0.1);
+    });
   };
 
-  var ajaxError = function(event) {
-    console.error(event);
+  var restoreTableRow = function(event) {
+    var $importButton = $(this);
+    var $row = $importButton.closest('tr');
+
+    $importButton.fadeTo('slow', 1.0, function() { $importButton.css('color', 'red') });
+
+    $.each(TABLE_ROW_FIELDS, function(i, field) {
+      var tdSelector = `td.${field}`;
+      var $td = $row.find(tdSelector);
+      $td.fadeTo('slow', 1);
+    });
   }
+
+  var updateTableRow = function(event) {
+    // For rails-ujs: https://stackoverflow.com/a/45632235/1093087
+    var $importButton = $(this);
+    var $row = $importButton.closest('tr');
+    var data = event.detail[0], status = event.detail[1], xhr = event.detail[2];
+
+    $importButton.fadeTo('slow', 1.0, function() { $importButton.css('color', 'green') });
+
+    // Update fields
+    $.each(TABLE_ROW_FIELDS, function(i, field) {
+      var tdSelector = `td.${field}`;
+      var $td = $row.find(tdSelector);
+      var value = data[field];
+      $td.val(value);
+      $td.fadeTo('slow', 1);
+    });
+
+    console.log(data);
+  };
 
   // Module Public API
   return {
