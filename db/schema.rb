63cb979ae59838eb38fcc63dcdd1b0fb2d76eb97
klenwell <klenwell@gmail.com>
2018-10-22 19:42:39 -0700

    Stubs out ScrumEvent model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_024110) do
+ActiveRecord::Schema.define(version: 2018_10_22_165446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,22 @@ ActiveRecord::Schema.define(version: 2018_09_03_024110) do
     t.datetime "last_imported_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "scrum_events", force: :cascade do |t|
+    t.string "eventable_type"
+    t.bigint "eventable_id"
+    t.bigint "scrum_board_id"
+    t.string "action"
+    t.string "trello_id"
+    t.string "trello_type"
+    t.string "trello_object"
+    t.json "trello_data"
+    t.datetime "occurred_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["eventable_type", "eventable_id"], name: "index_scrum_events_on_eventable_type_and_eventable_id"
+    t.index ["scrum_board_id"], name: "index_scrum_events_on_scrum_board_id"
   end
 
   create_table "scrum_sprints", force: :cascade do |t|
@@ -88,6 +104,7 @@ ActiveRecord::Schema.define(version: 2018_09_03_024110) do
   end
 
   add_foreign_key "scrum_backlogs", "scrum_boards"
+  add_foreign_key "scrum_events", "scrum_boards"
   add_foreign_key "scrum_sprints", "scrum_boards"
   add_foreign_key "wish_heaps", "scrum_boards"
 end
