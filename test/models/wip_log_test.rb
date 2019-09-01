13456fda8d7e8a362cd95ed68bf6d1cadaa9aff0
klenwell <klenwell@gmail.com>
2019-09-01 13:04:21 -0700

    Will reproduce undefined started_on method error for imports in WipLog test.

--- test/models/wip_log_test.rb
+++ test/models/wip_log_test.rb
@@ -1,7 +1,35 @@
+#
+# rake test TEST=test/models/wip_log_test.rb
+#
 require 'test_helper'
 
 class WipLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "expects to create WIP log" do
+    # Arrange
+    scrum_event = scrum_events(:creates_card)
+
+    # Act
+    wip_log = WipLog.create_from_event(scrum_event)
+
+    # Assert
+    assert_equal scrum_event, wip_log.event
+  end
+
+  test "expects to create WIP log even when board has no completed queues" do
+    # For more information, see https://github.com/klenwell/scrumwell/issues/36
+    # Arrange
+    board = ScrumBoard.create!(name: 'Test Board')
+    trello_import = TrelloImport.create!(scrum_board_id: board.id)
+    scrum_event = ScrumEvent.create!(trello_import_id: trello_import.id,
+                                     occurred_at: Time.zone.now - 1.day)
+
+    # Assume
+    assert scrum_event.board.completed_queues.first.nil?
+
+    # Act
+    wip_log = WipLog.create_from_event(scrum_event)
+
+    # Assert
+    assert_equal scrum_event, wip_log.event
+  end
 end
