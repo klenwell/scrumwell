255163856eda1628c2043416741a74c856194242
klenwell <klenwell@gmail.com>
2019-09-01 16:07:31 -0700

    Will replace references to Sidekiq::Logging.logger with Sidekiq.logger.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 # https://stackoverflow.com/a/18131117/1093087
 # https://github.com/mperham/sidekiq/issues/1682#issuecomment-103843390
 Sidekiq.configure_server do |config|
-  Rails.logger = Sidekiq::Logging.logger
-  ActiveRecord::Base.logger = Sidekiq::Logging.logger
+  Rails.logger = Sidekiq.logger
+  ActiveRecord::Base.logger = Sidekiq.logger
 end
 
 # Print backtraces when exceptions occur: https://github.com/mperham/sidekiq/issues/3135
